dfd6a8e1a2ec75d33b90a90a40ddfda636c369d6
helloppx <helloppx@gmail.com>
2014-06-05 18:32:10 +0800

    lsl

--- x.sh
+++ x.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/expect 
 set timeout 30 
-spawn git pull
-expect "password:" 
-send "ispass\r" 
+spawn git add .
+spawn git commit -m "lsl"
+spawn git push
+expect "Username" 
+send "helloppx@gmail.com\r" 
+expect "Password" 
+send "pythonalvin123\r" 
 interact 
